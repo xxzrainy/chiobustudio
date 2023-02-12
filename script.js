502e05e5ab96c14c68c03a8794fa4d8da9e5b5e7
xxzrainy <118530943+xxzrainy@users.noreply.github.com>
2023-02-13 00:25:24 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-window.onload = function(){    
-    document.getElementById(backToTop).onclick = f1
-    document.getElementById(test).onclick = f2
-}
+$(function(){ //(window).onload
+    
+    //--切換分頁
+    let blockA = $(".portfolio").offset().top - 60
+    let blockB = $(".service").offset().top - 60
+    let blockC = $(".about").offset().top
 
-f1.onclick = window.scrollTo(0, 0, smooth)
-f2.onclick = window.scrollTo(1000, 0, smooth)
+    $(".navBarBtn1").click(function(){
+        $("html,body").animate({scrollTop: blockA}, 500)
+    })
+
+    $(".navBarBtn2").click(function(){
+        $("html,body").animate({scrollTop: blockB}, 500)
+    })
+
+    $(".navBarBtn3").click(function(){
+        $("html,body").animate({scrollTop: blockC}, 500)
+    })
+
+    $(".kv").click(function(){
+        $("html,body").animate({scrollTop: 0}, 500)
+    })
+
+    //--服務項目說明文字替換
+})
